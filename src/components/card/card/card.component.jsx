9aad55dbd339ce093508d7350985badbabab4215
Emmanuel Omole <eomole@systemspecs.com.ng>
2021-12-21 17:19:31 +0100

    adding files for github pages

---
+++ src/components/card/card/card.component.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import './card.styles.css'
+
+const Card = (props) => {
+    return(
+    <div className='card-container'>
+        <img alt='' src={`https://robohash.org/&${props.monster.id}?set=set2&size=180x180`} />
+    <div><h2>{props.monster.name}</h2>
+    <p>{props.monster.email}</p></div>
+    </div>
+)}
+
+export default Card
